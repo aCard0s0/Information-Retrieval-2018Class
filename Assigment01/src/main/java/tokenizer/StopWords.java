150def5ef5d1aee247474eab28c9e1381e95ac60
ivo <ivoangelico@ua.pt>
2018-10-21 18:49:33 +0100

    missing stopwords files

---
+++ Assigment01/src/main/java/tokenizer/StopWords.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tokenizer;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ivo
+ */
+public class StopWords {
+    private List<String> stopWords;
+    private final String DEFAULT_PATH = "src/main/java/tokenizer/stopwords.txt"; 
+    private final int LIMIT_SIZE = 3;
+
+    public StopWords(){
+        this.stopWords = new ArrayList<String>();
+        try {
+            BufferedReader swReader = new BufferedReader(new FileReader(DEFAULT_PATH));
+            String s;
+            while((s=swReader.readLine())!=null) {
+                if(s.contains("'")){ //get reed of the contraction
+                    if(s.split("'", 2)[1].length()<3){
+                        s = s.split("'", 2)[0];
+                    }
+                }
+                if(s.length() > LIMIT_SIZE){
+                    this.stopWords.add(s);
+                }
+            }
+            swReader.close();
+        } catch (FileNotFoundException ex1) {
+            ex1.printStackTrace();
+        } catch (IOException ex2) {
+            ex2.printStackTrace();
+        }
+    }
+    public boolean check(String token)
+    {
+        return this.stopWords.contains(token);
+    }
+}
